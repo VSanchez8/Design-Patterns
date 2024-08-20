182bc52cb5b6f93d40e950527a459e8ee2f96de8
Ivan-Axity <139185208+Ivan-Axity@users.noreply.github.com>
2024-08-20 15:59:15 +0000

    uso del servicio de msn

--- Creacionales/Singleton/SMSNotificationService.ts
+++ Creacionales/Singleton/SMSNotificationService.ts
@@ -3,6 +3,7 @@ import { INotificationService } from './INotificationService';
 
 export class SMSNotificationService implements INotificationService {
     public sendNotification(message: string, recipient: string): void {
+        console.log("Conectando con el proveedor de telefonía");
         console.log(`Enviando SMS a ${recipient}: ${message}`);
     }
 }
